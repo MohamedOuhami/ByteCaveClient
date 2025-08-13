217435be4081794ba51ae4114ee836af977f8efa
MohamedOuhami <mohamed.ouhami2001@gmail.com>
2025-08-13 16:12:47 +0100

    Started the project

---
+++ build/Desktop-Debug/CMakeFiles/ByteCaveClient.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "ByteCaveClient_autogen"
+  "CMakeFiles/ByteCaveClient.dir/link.d"
+  "CMakeFiles/ByteCaveClient_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/ByteCaveClient_autogen.dir/ParseCache.txt"
+  "ByteCaveClient"
+  "ByteCaveClient.pdb"
+  "ByteCaveClient_autogen/mocs_compilation.cpp"
+  "ByteCaveClient_autogen/timestamp"
+  "CMakeFiles/ByteCaveClient.dir/ByteCaveClient_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/ByteCaveClient.dir/ByteCaveClient_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/ByteCaveClient.dir/main.cpp.o"
+  "CMakeFiles/ByteCaveClient.dir/main.cpp.o.d"
+  "CMakeFiles/ByteCaveClient.dir/mainwindow.cpp.o"
+  "CMakeFiles/ByteCaveClient.dir/mainwindow.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ByteCaveClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop-Debug/CMakeFiles/ByteCaveClient_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ByteCaveClient_autogen/include/ui_mainwindow.h"
+  "ByteCaveClient_autogen/mocs_compilation.cpp"
+  "ByteCaveClient_autogen/timestamp"
+  "CMakeFiles/ByteCaveClient_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ByteCaveClient_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
